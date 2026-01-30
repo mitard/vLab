5d532ddb4f1701701d584bdd90cef11b46bd4db9
Vincent Mitard <vincent.mitard@proton.me>
2026-01-30 21:41:49 +0100

    Mise à jour du fichier d'initialisation de la VM Ansible

--- hostFiles/ansible/initialStartup.sh
+++ hostFiles/ansible/initialStartup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 bannerFile=/etc/ssh/ssh_banner
+ansibleEnvURL="https://raw.githubusercontent.com/mitard/vLab/refs/heads/main/hostFiles/ansible/AnsibleEnv.tar.gz"
+ansibleEnv="/var/tmp/AnsibleEnv.tar.gz"
 
-dpkg -i /var/python3-proxmoxer_2.2.0-2_all.deb
-ansible-galaxy collection install git+https://github.com/ansible-collections/community.proxmox.git -p /usr/share/ansible/collections
-tar -gunzip --extract --absolute-names --file /var/AnsibleEnv.tar.gz
 echo "" > $bannerFile
 figlet -c -f slant -k "Pod ${HOSTNAME: -1}" >> $bannerFile
 echo "" >> $bannerFile
 figlet -c -f term 'Serveur Ansible (Automatisation de la gestion du lab)' >> $bannerFile
 echo "" >> $bannerFile
 echo "PodID=${HOSTNAME: -1}" >> ~ansible/.profile
+
+# Installation du paquet promoxer v2 nécessaire pour le module community.proxmox
+dpkg -i /var/python3-proxmoxer_2.2.0-2_all.deb
+ansible-galaxy collection install git+https://github.com/ansible-collections/community.proxmox.git -p /usr/share/ansible/collections
+
+# Installation de l'environnement Ansible pour l'utilisateur ansible
+curl --location $ansibleEnvURL --output $ansibleEnv
+tar -gunzip --extract --absolute-names --file $ansibleEnv
 
 chown ansible:ansible /home/ansible/.ansible
 
